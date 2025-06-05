2b9662a1d144cebe2f7915aabdb711e49148a9aa
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-06-05 16:04:25 +0300

    Add main.c with a simple Hello World program

---
+++ 01-First_Program/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include standard input/output library
+#include <stdlib.h> // Include standard library for exit status
+
+int main() {
+    
+    printf("Hello, World!\n"); // Print a message to the console
+    return EXIT_SUCCESS; // Return success status
+}
