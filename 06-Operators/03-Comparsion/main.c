30ef430d7da6574caaf758d8a0c9a2549808dc98
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-06-06 12:28:33 +0300

    Add main.c with comparison operators and output

---
+++ 06-Operators/03-Comparsion/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    // Comparison operators
+    int a = 10, b = 20;
+    printf("a: %d, b: %d\n", a, b);
+    printf("a == b: %d\n", a == b); // Equal to
+    printf("a != b: %d\n", a != b); // Not equal to
+    printf("a > b: %d\n", a > b);   // Greater than
+    printf("a < b: %d\n", a < b);   // Less than
+
+    return 0;
+}
