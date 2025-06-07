99bdbe84c57ad2375935946b354011f20b59837d
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-06-07 11:34:18 +0300

    Fix formatting by adding a newline at the end of main.c

--- 08-Conditions/04-Lower_upper_case_example/main.c
+++ 08-Conditions/04-Lower_upper_case_example/main.c
@@ -10,6 +10,6 @@ int main(){
     // Check if the character is in lower case or upper case
     (c >= 'a' && c <= 'z') ? printf("The character '%c' is in lower case.\n", c) : (c >= 'A' && c <= 'Z') ? printf("The character '%c' is in upper case.\n", c) : printf("The character '%c' is neither in lower case nor in upper case.\n", c);
 
-
+    
     return 0;
 }
