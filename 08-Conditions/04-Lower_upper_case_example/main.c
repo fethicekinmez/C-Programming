1ed4d531c1272a6b29c28b0164a7043692165242
Fethi Çekinmez <fethicekinmez@gmail.com>
2025-06-07 11:31:54 +0300

    Finding if a letter is Lowercase or Uppercase

---
+++ 08-Conditions/04-Lower_upper_case_example/main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+    char c;
+    printf("Enter a character: ");
+    scanf(" %c", &c);
+
+    // Check if the character is in lower case or upper case
+    (c >= 'a' && c <= 'z') ? printf("The character '%c' is in lower case.\n", c) : (c >= 'A' && c <= 'Z') ? printf("The character '%c' is in upper case.\n", c) : printf("The character '%c' is neither in lower case nor in upper case.\n", c);
+
+
+    return 0;
+}
